8bb555da6183beb82e4d417f9c60aa7781bc0fb1
tonirvega <arvegadev@hotmail.com>
2024-07-23 02:03:18 +0200

    guessing game

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+  println!("Guess the number!");
+  
+  let secret_number = rand::thread_rng().gen_range(1..=100);
+
+  println!("The secret number is: {secret_number}");
+  
+  loop {
+  
+     println!("Please input your guess.");
+   
+     let mut guess = String::new();
+   
+     io::stdin()
+         .read_line(&mut guess)
+         .expect("Failed to read line");
+         println!("You guessed: {guess}");
+   
+     // redeclare var and use .parse method to cast var
+     let guess: u32 = match guess.trim().parse(){
+       
+       Ok(num) => num,
+      
+       Err(_) =>{
+    
+         println!("❌ Not a number! Try with a valid input");
+        
+         continue;
+        
+       },
+     };
+     
+     match guess.cmp(&secret_number) {
+         
+         Ordering::Less => println!("🌭 Too small!"),
+         
+         Ordering::Greater => println!("🐕 Too big!"),
+       
+         Ordering::Equal => {
+             println!("🎉🎉🎉 You win!");
+             break;
+        }
+     }
+   
+    println!("You guessed: {guess}");
+
+  }
+}
